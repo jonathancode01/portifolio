a3b33aff0a973822ff1cf18450d4535bd685394a
jonathancode01 <icontatojonathan@gmail.com>
2024-07-23 11:20:15 -0400

    feat:novas alterações

---
+++ script.js
@@ -0,0 +1,32 @@
+// modelos de calculadora
+// let n1 = prompt("Adição: ");
+// n1 = Number(n1);
+// let n2 = prompt("Subtração: ");
+// n2 = Number(n2);
+// alert("A soma e: " + (n1 + n2));
+// alert("A subtração e: " + (n1 - n2));
+// alert("A multiplicação e: " + (n1 * n2));
+// alert("A divisão e: " + (n1 / n2));
+
+//
+
+// media
+// let numero = parseFloat(prompt("digite um numero: "));
+// let numero2 = parseFloat(prompt("digite outro numero: "))
+// let media = (numero + numero2) / 2;
+// alert("A media " + media);
+
+// let n = 10;
+// n += 5;
+// n*= 5;
+// n++;
+// alert("O resultado da divisão  de " + n + " por 6 e: " + (n % 6));
+// alert("O resto da divisãode " + n + " por 6 e: " + (n % 6) ** 2);
+// alert(n)
+// n /= 4;
+// alert(n)
+
+let jonathan = "Ow";
+
+jonathan += parseInt(jonathan); // atribuição +=
+alert(jonathan)
