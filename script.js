2c7f3a6cf261315b30ecf62a08016653ff09d719
jonathancode01 <icontatojonathan@gmail.com>
2024-07-24 16:41:58 -0400

    atualização

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// modelos de calculadora
+// #region calculadora
 // let n1 = prompt("Adição: ");
 // n1 = Number(n1);
 // let n2 = prompt("Subtração: ");
@@ -8,12 +8,13 @@
 // alert("A multiplicação e: " + (n1 * n2));
 // alert("A divisão e: " + (n1 / n2));
 
-// media
+// #region media
 // let numero = parseFloat(prompt("digite um numero: "));
 // let numero2 = parseFloat(prompt("digite outro numero: "))
 // let media = (numero + numero2) / 2;
 // alert("A media " + media);
 
+// #region resto da divisao
 // let n = 10;
 // n += 5;
 // n*= 5;
@@ -24,32 +25,116 @@
 // n /= 4;
 // alert(n)
 
+// #region minuscula
 // let jonathan = "Ow";
 // jonathan += parseInt(jonathan); // atribuição +=
 // alert(jonathan.toLowerCase()); // Colocar as letras minusculas "toLowCase"
 
+// #region maior
 // let numero1 = parseFloat(prompt("digite um numero de 0 a 10: "));
 // let numero2 = parseFloat(prompt("digite outro numero de 0 a 10: "));
 // // pra ser executado antes a operação logica, tem que colocar entre parenteses
 // alert("numeoro 1 e maior retorne: true, senão for retorne false: " + (numero1 >= numero2))
 
+
 // let num = parseInt(prompt("digite um numero de 0 a 10: "));
 // let num2 = parseInt(prompt("digite outro numero de 0 a 10: "));
-
 // let media = (num + num2) / 2;
-
 // alert("A media e menor ou igual a 4? " + (media < 4 || num === 0 || num2 === 0)); 
 // // caso um unico numero for 0 ele vai retornar false
 
 
 
-let n1 = parseInt(prompt("digite um numero de 0 a 10: "));
-let n2 = parseInt(prompt("digite outro numero de 0 a 10: "));
+// let n1 = parseInt(prompt("digite um numero de 0 a 10: "));
+// let n2 = parseInt(prompt("digite outro numero de 0 a 10: "));
+// let media = (n1 + n2) / 2;
+// if (media > 4 && n1 > 0 && n2 > 0) {
+//     alert("True")
+// } else {
+//     alert("False")
+// }
 
-let media = (n1 + n2) / 2;
 
-if (media > 4){
-    console.log("A media e maior que 4");
-} else {
-    console.log("Reprovado");
+// let n1 = parseInt(prompt("digite um numero de 0 a 10: "));
+// let n2 = parseInt(prompt("digite outro numero de 0 a 10: "));
+// //mostrar aprovado ou reprovado de acordo com a media 
+// if (n1 <= 10 && n2 <= 10){
+//     let media = (n1 + n2) / 2;
+//      if (media > 4 && n1 > 0 && n2 > 0) {
+//         alert("aprovado")
+//     }else {
+//         alert("reprovado")
+//     }
+// } else {
+//     alert("digite o numero certo")
+// }
+
+// let n1 = parseFloat(prompt("digite um numero de 0 a 10: "))
+// let n2 = parseFloat(prompt("digite outro numero de 0 a 10: "))
+
+// if (n1 <= 10 && n2 <= 10 && n1 >= 0 && n2 >= 0) {
+//     let media = (n1 + n2) / 2
+//     if (media > 4 && n1 > 0 && n2 > 0) {
+//         alert("Aprovado")
+//     } else {
+//         alert("Reprovado")
+//     }
+// } else {
+//     alert("digite o numero certo")
+// }
+
+// #region letra n numero
+// let n1 = parseInt(prompt("digite um numero de 0 a 10: "));
+// let n2 = parseInt(prompt("digite outro numero de 0 a 10: "));
+// if (n1 <= 10 && n2 <= 10 && n1 >= 0 && n2 >= 0 && !isNaN(n1) && !isNaN(n2) && n1 != "" && n2 != "") {
+//     let media = (n1 + n2) / 2
+//     if (media > 4 && n1 > 0 && n2 > 0) {
+//         alert("Aprovado")
+//     } else {
+//         alert("Reprovado")
+//     }
+// } else {
+//     alert("digite o numero certo")
+// }
+
+// switch case PARA DIAS DA SEMANA
+// #region switch case
+// let dia = prompt("digite um numero da semana: ");
+// switch (dia) {
+//     case "1":
+//     alert("segunda");
+//          break;
+//      case "2":
+//          alert("terca");
+//          break;
+//      case "3":
+//          alert("quarta");
+//          break;
+//      case "4":
+//          alert("quinta");
+//          break;
+//      case "5":
+//          alert("sexta");
+//          break;
+//      case "6":
+//          alert("sabado");
+//         case "7":
+//         alert("domingo");
+//          break;
+//      default:
+//          alert("digite o dia certo");
+// }
+
+
+let data = parseFloat(prompt("Digite uma variavel: "));
+
+switch (data) {
+    case isNaN(data):
+        alert("Dia da semana");
+        break;
+    case :
+        alert("Numero");
+        break;
+    default:
+        alert("Data: ");
 }
