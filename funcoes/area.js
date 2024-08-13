439718d8f3441e552ffb50f0bce95c11687c0756
jonathancode01 <icontatojonathan@gmail.com>
2024-08-13 16:55:05 -0400

    Ops

--- funcoes/area.js
+++ funcoes/area.js
@@ -4,3 +4,17 @@ function calcularAreaRetangulo(l, a){
     }
     return l * a;
 }
+
+function calcularAreaTriangulo(b, a){
+    if(typeof b !== "number" || typeof a !== "number"){
+        throw new Error("Os valores precisam ser numéricos")
+    }
+    return (b * a) / 2;
+}
+
+function calcularAreaCirculo(c){
+    if(typeof c !== "number"){
+        throw new Error("Os valores precisam ser numéricos")
+    }
+    return Math.PI * (c * c)
+}
